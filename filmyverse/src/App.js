46b0f9fd7931207339ab0fe0c8a991387778a239
Mohammed Wajid <wajid17023@gmail.com>
2023-04-20 15:28:37 +0530

    making header section

--- filmyverse/src/App.js
+++ filmyverse/src/App.js
@@ -1,8 +1,9 @@
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-blue-500">filmyverse</h1>
+      <Header />
     </div>
   );
 }
---
+++ filmyverse/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='text-2xl'>Filmy
+    <span className='text-red-500'>verse</span></div>
+  )
+}
+
+export default Header
