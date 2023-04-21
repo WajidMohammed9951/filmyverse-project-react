5fbbfaebc646ae721c237a9e0e44368c036b0868
Mohammed Wajid <wajid17023@gmail.com>
2023-04-21 23:24:27 +0530

    made some changes ing header section

--- filmyverse/src/components/Header.js
+++ filmyverse/src/components/Header.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 const Header = () => {
   return (
+
+    
     <div className='text-3xl text-red-500 font-bold p-3 border-b-2 border-gray-500'>Filmy
     <span className='text-white'>verse</span></div>
   )
