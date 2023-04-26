68bdeb736b183fe12382d3e2bd3ae28649fc9081
Mohammed Wajid <wajid17023@gmail.com>
2023-04-26 13:15:45 +0530

    installed icons

--- filmyverse/src/components/Header.js
+++ filmyverse/src/components/Header.js
@@ -4,8 +4,11 @@ const Header = () => {
   return (
 
     
-    <div className='text-3xl text-red-500 font-bold p-3 border-b-2 border-gray-500'>Filmy
-    <span className='text-white'>verse</span></div>
+    <div className='text-3xl text-red-500  flex justify-between items-center font-bold p-3 border-b-2 border-gray-500'><span>Filmy
+    <span className='text-white'>verse</span></span>
+    <h1 className='text-lg'>Add New</h1>
+    
+    </div>
   )
 }
 
