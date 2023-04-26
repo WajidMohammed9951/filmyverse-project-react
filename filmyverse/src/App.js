5e91aed967f68e4992fc916d40f3084b31fdc535
Mohammed Wajid <wajid17023@gmail.com>
2023-04-26 15:51:19 +0530

    creating card.js

--- filmyverse/src/App.js
+++ filmyverse/src/App.js
@@ -1,11 +1,12 @@
 import Header from "./components/Header";
-
+import Card from "./components/Card";
 function App() {
   return (
     <div className="App">
 
       
       <Header />
+      <Card />
     </div>
   );
 }
