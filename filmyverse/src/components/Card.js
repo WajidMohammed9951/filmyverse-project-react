f1a1b051c11c6433836ad39ecba4b4cf6e2e5412
Mohammed Wajid <wajid17023@gmail.com>
2023-04-28 17:04:25 +0530

    adding  map function

--- filmyverse/src/components/Card.js
+++ filmyverse/src/components/Card.js
@@ -50,12 +50,12 @@ const Card = () => {
   ])
   return (
 
-    <div className='flex flex-wrap justify-between font-bold p-3 mt-7' >
+    <div className='flex flex-wrap justify-between font-bold p-3 mt-3' >
       {data.map((e, i) => {
         return (
-          <div className='Card'>
-            <img className='h-80 p-1 shadow-lg cursor-pointer hover:translate-y-1 ' src={e.img} />
-            <h1><span className='text-gray-500'>Name : </span>{e.name}r</h1>
+          <div key={i} className='Card p-1 shadow-lg cursor-pointer hover:translate-y-1 mt-6'>
+            <img className='h-80' src={e.img} />
+            <h1><span className='text-gray-500'>Name : </span>{e.name}</h1>
             <h1><span className='text-gray-500'>Rating : </span>{e.Rating}</h1>
             <h1><span className='text-gray-500'>Year : </span>{e.Year}</h1>
           </div>
