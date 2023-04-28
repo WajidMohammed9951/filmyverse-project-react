7137f7453c980f0ee6bde9e4f8fc8603c7c1cbda
Mohammed Wajid <wajid17023@gmail.com>
2023-04-28 16:55:52 +0530

    adding different movies

--- filmyverse/src/App.js
+++ filmyverse/src/App.js
@@ -1,5 +1,7 @@
 import Header from "./components/Header";
 import Card from "./components/Card";
+
+
 function App() {
   return (
     <div className="App">
--- filmyverse/src/components/Card.js
+++ filmyverse/src/components/Card.js
@@ -1,10 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Card = () => {
-  return (
-    <div>Card</div>
-
+  const [data, setdata] = useState([
+    {
+      name: "Black Panther",
+      Year: "2018",
+      Rating: "5",
+      img: "https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg",
+    },
+    {
+      name: "Black Panther",
+      Year: "2018",
+      Rating: "5",
+      img: "https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg",
+    },
+    {
+      name: "Black Panther",
+      Year: "2018",
+      Rating: "5",
+      img: "https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg",
+    },
+    {
+      name: "Black Panther",
+      Year: "2018",
+      Rating: "5",
+      img: "https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg",
+    },
+    {
+      name: "Black Panther",
+      Year: "2018",
+      Rating: "5",
+      img: "https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg",
+    },
+    {
+      name: "Black Panther",
+      Year: "2018",
+      Rating: "5",
+      img: "https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg",
+    },
+    {
+      name: "Black Panther",
+      Year: "2018",
+      Rating: "5",
+      img: "https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg",
+    },
     
+
+
+  ])
+  return (
+
+    <div className='flex flex-wrap justify-between font-bold p-3 mt-7' >
+      {data.map((e, i) => {
+        return (
+          <div className='Card'>
+            <img className='h-80 p-1 shadow-lg cursor-pointer hover:translate-y-1 ' src={e.img} />
+            <h1><span className='text-gray-500'>Name : </span>{e.name}r</h1>
+            <h1><span className='text-gray-500'>Rating : </span>{e.Rating}</h1>
+            <h1><span className='text-gray-500'>Year : </span>{e.Year}</h1>
+          </div>
+        )
+      })}
+    </div>
+
+
   )
 }
 
