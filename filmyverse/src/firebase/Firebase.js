7f56005d65d512d24b477e099ca373fd06df5dd4
Mohammed Wajid <wajid17023@gmail.com>
2023-05-05 20:30:18 +0530

    adding data to firebase completed

---
+++ filmyverse/src/firebase/Firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore, collection} from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA97F5PS7wqvUdJdyakmaJnqq3gYdaSQSg",
+  authDomain: "filmyverse-9590d.firebaseapp.com",
+  projectId: "filmyverse-9590d",
+  storageBucket: "filmyverse-9590d.appspot.com",
+  messagingSenderId: "194704483504",
+  appId: "1:194704483504:web:b596170dd334a42b318775"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const moviesRef = collection(db, "movies");
+export default app;
