341c264048c293751a440f1d0f3bb01c27a83867
Mohammed Wajid <wajid17023@gmail.com>
2023-05-14 01:59:51 +0530

    styling review.js

--- filmyverse/src/components/Reviews.js
+++ filmyverse/src/components/Reviews.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Reviews = () => {
   return (
-    <div>Reviews</div>
+    <div className='mt-6 w-full text-xl'>
+      <input
+      placeholder='share your thoughts...'
+      className='p-2 w-full outline-none bg-slate-900'
+      />
+      <button className='bg-green-500 p-1 w-full mt-1'>share</button>
+    </div>
   )
 }
 
