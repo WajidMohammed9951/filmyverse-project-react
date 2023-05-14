8ebaaddd1f72fee8e0914d716a76a787b31c7326
Mohammed Wajid <wajid17023@gmail.com>
2023-05-15 00:54:09 +0530

    storing reviews data

--- filmyverse/src/components/Reviews.js
+++ filmyverse/src/components/Reviews.js
@@ -3,26 +3,64 @@ import ReactStars from 'react-stars'
 import { reviewsRef } from '../firebase/Firebase';
 import { addDoc } from 'firebase/firestore';
 import { TailSpin } from 'react-loader-spinner';
+import swal from 'sweetalert';
 
-const Reviews = () => {
+
+const Reviews = ({ id }) => {
   const [rating, setRating] = useState(0);
   const [loading, setLoading] = useState(false);
+  const [form, setForm] = useState("");
+
+  const sendReview = async () => {
+    setLoading(true);
+
+    try {
+      await addDoc(reviewsRef, {
+        movieid: id,
+        name: "wajid",
+        rating: rating,
+        thought: form,
+        timestamp: new Date().getTime()
+
+      })
+
+
+      swal({
+        title: "review sent",
+        icon: "success",
+        buttons: false,
+        timer: 3000
+      })
+    } catch (error) {
+      swal({
+        title: error.message,
+        icon: "error",
+        buttons: false,
+        timer: 3000
+      })
+    }
+    setLoading(false);
+
+  }
+
   return (
     <div className='mt-6 border-t-2 border-gray-600 w-full text-xl '>
       <ReactStars
-      size={30}
-      half={true}
-      value={rating}
-      onChange={(rate) => setRating(rate)}
-      
-       className='mt-3'/>
+        size={30}
+        half={true}
+        value={rating}
+        onChange={(rate) => setRating(rate)}
+
+        className='mt-3' />
       <input
-      placeholder='share your thoughts...'
-      className='p-2 w-full outline-none bg-slate-900 mt-2'
+        value={form}
+        onChange={(e => setForm(e.target.value))}
+        placeholder='share your thoughts...'
+        className='p-2 w-full outline-none bg-slate-900 mt-2'
       />
-      <button className='bg-green-500 flex justify-center p-2 w-full mt-1'>
-       {loading ? <TailSpin height={20} color='white'/>  :'share'}
-        </button>
+      <button onClick={sendReview} className='bg-green-500 flex justify-center p-2 w-full mt-1'>
+        {loading ? <TailSpin height={20} color='white' /> : 'share'}
+      </button>
     </div>
   )
 }
